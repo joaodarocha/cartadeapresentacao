0a6bea9ce26a66950a245fd6bcca2be118da7872
João Rocha <joaorocha@storyteq.com>
2025-07-14 09:46:13 +0100

    feat: add Google Analytics

--- src/env.ts
+++ src/env.ts
@@ -2,10 +2,10 @@ import * as z from 'zod'
 
 import { defineEnvValidationSchema } from 'wasp/env'
 
-export const envValidationSchema = defineEnvValidationSchema(
+export const envValidationSchema =
   z.object({
     REACT_APP_ANALYTICS_ID: z.string({
       required_error: 'REACT_APP_ANALYTICS_ID is required.',
     }),
-  })
-)
+  });
+
